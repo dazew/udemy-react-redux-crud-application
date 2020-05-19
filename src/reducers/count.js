0b47a1d2890670549846365ddbc2b984b34dcfa6
eja <ejaeja@eja.jp>
2020-05-19 22:07:24 +0900

    implement reducer

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -6,9 +6,10 @@ export default (state = initialState, action) => {
     siwtch(action.type){
         case INCREMENT:
             return { value: state.value + 1};
-            break;
         case DECREMENT:
             return { value: state.value - 1};
+        default:
+            return state;
     }
 
 }
